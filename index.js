4ea9cbfc6e0fc196115fa47d0478028e7e429945
Alex Hall <me@jalexhall.com>
2022-02-08 19:22:36 -0500

    More plumbing

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express")
-const fs = require('fs')
 
 const port = 8000;
 
 const webservice = express()
 
 console.log(`Starting listener on port ${port}.`)
-webservice.listen(port, () => {
-	console.log(`Ring Bridge is running on port ${port}.`);
-})
+
+webservice.listen(port, () => {})
