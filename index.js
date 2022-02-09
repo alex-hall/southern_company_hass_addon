91ff2ef00965f9cc1fb8213b74ad94bc6ce91a98
Alex Hall <me@jalexhall.com>
2022-02-08 20:10:35 -0500

    Wire up ENVs + Test API e2e

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const express = require("express")
+require('dotenv').config()
 
+const express = require("express")
+const {SouthernCompanyAPI} = require("southern-company-api")
 const port = 8000;
+
 
 const app = express()
 
 console.log(`Starting listener on port ${port}.`)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const config = {
+    username: process.env.username,
+    password: process.env.password,
+    account: process.env.account
+}
+
+const api = new SouthernCompanyAPI(config)
+
+api.on('connected', async () => {
+    const response = await api.getAccounts()
+    console.log("Hello from get accounts: " + response)
+});
+
+app.get('/', async (req, res) => {
+    res.send("HELLO WORLD")
+
 })
 
-app.listen(port, () => {})
+app.listen(port, () => {
+})
